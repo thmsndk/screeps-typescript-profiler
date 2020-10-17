86d863ea4d220e6afc7361b98d3494325b426a9b
thmsn <9084377+thmsndk@users.noreply.github.com>
2020-10-18 01:16:43 +0200

    formatted code

--- Profiler/Profiler.ts
+++ Profiler/Profiler.ts
@@ -2,16 +2,20 @@
 export function init(): Profiler {
   const defaults = {
     data: {},
-    total: 0,
+    total: 0
   };
 
-  if (!Memory.profiler) { Memory.profiler = defaults; }
+  if (!Memory.profiler) {
+    Memory.profiler = defaults;
+  }
 
   const cli: Profiler = {
     clear() {
       const running = isEnabled();
       Memory.profiler = defaults;
-      if (running) { Memory.profiler.start = Game.time; }
+      if (running) {
+        Memory.profiler.start = Game.time;
+      }
       return "Profiler Memory cleared";
     },
 
@@ -33,20 +37,24 @@ export function init(): Profiler {
     },
 
     stop() {
-      if (!isEnabled()) { return; }
+      if (!isEnabled()) {
+        return;
+      }
       const timeRunning = Game.time - Memory.profiler.start!;
       Memory.profiler.total += timeRunning;
       delete Memory.profiler.start;
       return "Profiler stopped";
     },
-    
+
     toString() {
-       return "Profiler.start() - Starts the profiler\n" +
-          "Profiler.stop() - Stops/Pauses the profiler\n" +
-          "Profiler.status() - Returns whether is profiler is currently running or not\n" +
-          "Profiler.output() - Pretty-prints the collected profiler data to the console\n" +
-          this.status();
-     },
+      return (
+        "Profiler.start() - Starts the profiler\n" +
+        "Profiler.stop() - Stops/Pauses the profiler\n" +
+        "Profiler.status() - Returns whether is profiler is currently running or not\n" +
+        "Profiler.output() - Pretty-prints the collected profiler data to the console\n" +
+        this.status()
+      );
+    }
   };
 
   return cli;
@@ -54,26 +62,36 @@ export function init(): Profiler {
 
 function wrapFunction(obj: object, key: PropertyKey, className?: string) {
   const descriptor = Reflect.getOwnPropertyDescriptor(obj, key);
-  if (!descriptor || descriptor.get || descriptor.set) { return; }
+  if (!descriptor || descriptor.get || descriptor.set) {
+    return;
+  }
 
-  if (key === "constructor") { return; }
+  if (key === "constructor") {
+    return;
+  }
 
   const originalFunction = descriptor.value;
-  if (!originalFunction || typeof originalFunction !== "function") { return; }
+  if (!originalFunction || typeof originalFunction !== "function") {
+    return;
+  }
 
   // set a key for the object in memory
-  if (!className) { className = obj.constructor ? `${obj.constructor.name}` : ""; }
+  if (!className) {
+    className = obj.constructor ? `${obj.constructor.name}` : "";
+  }
   const memKey = className + `:${key}`;
 
   // set a tag so we don't wrap a function twice
   const savedName = `__${key}__`;
-  if (Reflect.has(obj, savedName)) { return; }
+  if (Reflect.has(obj, savedName)) {
+    return;
+  }
 
   Reflect.set(obj, savedName, originalFunction);
 
-  ///////////
+  // /////////
 
-  Reflect.set(obj, key, function(this: any, ...args: any[]) {
+  Reflect.set(obj, key, function (this: any, ...args: any[]) {
     if (isEnabled()) {
       const start = Game.cpu.getUsed();
       const result = originalFunction.apply(this, args);
@@ -90,9 +108,11 @@ export function profile(target: object, key: string | symbol, _descriptor: Typed
 export function profile(
   target: object | Function,
   key?: string | symbol,
-  _descriptor?: TypedPropertyDescriptor<Function>,
+  _descriptor?: TypedPropertyDescriptor<Function>
 ): void {
-  if (!__PROFILER_ENABLED__) { return; }
+  if (!__PROFILER_ENABLED__) {
+    return;
+  }
 
   if (key) {
     // case of method decorator
@@ -103,13 +123,14 @@ export function profile(
   // case of class decorator
 
   const ctor = target as any;
-  if (!ctor.prototype) { return; }
+  if (!ctor.prototype) {
+    return;
+  }
 
   const className = ctor.name;
-  Reflect.ownKeys(ctor.prototype).forEach((k) => {
+  Reflect.ownKeys(ctor.prototype).forEach(k => {
     wrapFunction(ctor.prototype, k, className);
   });
-
 }
 
 function isEnabled(): boolean {
@@ -120,7 +141,7 @@ function record(key: string | symbol, time: number) {
   if (!Memory.profiler.data[key]) {
     Memory.profiler.data[key] = {
       calls: 0,
-      time: 0,
+      time: 0
     };
   }
   Memory.profiler.data[key].calls++;
@@ -141,13 +162,13 @@ function outputProfilerData() {
     totalTicks += Game.time - Memory.profiler.start;
   }
 
-  ///////
+  // /////
   // Process data
-  let totalCpu = 0;  // running count of average total CPU use per tick
+  let totalCpu = 0; // running count of average total CPU use per tick
   let calls: number;
   let time: number;
   let result: Partial<OutputData>;
-  const data = Reflect.ownKeys(Memory.profiler.data).map((key) => {
+  const data = Reflect.ownKeys(Memory.profiler.data).map(key => {
     calls = Memory.profiler.data[key].calls;
     time = Memory.profiler.data[key].time;
     result = {};
@@ -162,14 +183,14 @@ function outputProfilerData() {
 
   data.sort((lhs, rhs) => rhs.cpuPerTick - lhs.cpuPerTick);
 
-  ///////
+  // /////
   // Format data
   let output = "";
 
   // get function name max length
-  const longestName = (_.max(data, (d) => d.name.length)).name.length + 2;
+  const longestName = _.max(data, d => d.name.length).name.length + 2;
 
-  //// Header line
+  // // Header line
   output += _.padRight("Function", longestName);
   output += _.padLeft("Tot Calls", 12);
   output += _.padLeft("CPU/Call", 12);
@@ -177,17 +198,17 @@ function outputProfilerData() {
   output += _.padLeft("CPU/Tick", 12);
   output += _.padLeft("% of Tot\n", 12);
 
-  ////  Data lines
-  data.forEach((d) => {
+  // //  Data lines
+  data.forEach(d => {
     output += _.padRight(`${d.name}`, longestName);
     output += _.padLeft(`${d.calls}`, 12);
     output += _.padLeft(`${d.cpuPerCall.toFixed(2)}ms`, 12);
     output += _.padLeft(`${d.callsPerTick.toFixed(2)}`, 12);
     output += _.padLeft(`${d.cpuPerTick.toFixed(2)}ms`, 12);
-    output += _.padLeft(`${(d.cpuPerTick / totalCpu * 100).toFixed(0)} %\n`, 12);
+    output += _.padLeft(`${((d.cpuPerTick / totalCpu) * 100).toFixed(0)} %\n`, 12);
   });
 
-  //// Footer line
+  // // Footer line
   output += `${totalTicks} total ticks measured`;
   output += `\t\t\t${totalCpu.toFixed(2)} average CPU profiled per tick`;
   console.log(output);
